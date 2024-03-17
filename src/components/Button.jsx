a73ae09eb0275738accf7f104f6cece8904b6bce
kumarpritam1468 <manoharipritam@gmail.com>
2024-03-17 17:34:32 +0530

    add super quality, services section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,7 @@ const Button = ({ label, iconUrl }) => {
     <button className=' flex justify-center items-center gap-2 px-7 py-4 font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red'>
       {label}
 
-      <img src={iconUrl} alt="" className=' ml-2 rounded-full w-5 h-5' />
+      {iconUrl && <img src={iconUrl} alt="" className=' ml-2 rounded-full w-5 h-5' />}
     </button>
   )
 }
--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { services } from '../constants'
+import ServiceCard from '../components/ServiceCard'
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <section className=' max-container flex justify-center flex-wrap gap-9'>
+      {services.map((service, index) => (
+        <ServiceCard key={index} label={service.label} imgUrl={service.imgURL} subText={service.subtext} />
+      ))}
+    </section>
   )
 }
 
