40a1a684371b22ef924318bd108dd1e486072a7e
kumarpritam1468 <manoharipritam@gmail.com>
2024-03-19 21:20:52 +0530

    add Subscribe and footer section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Button = ({ label, iconUrl, backgroundColor, borderColor, textColor }) => {
+const Button = ({ label, iconUrl, backgroundColor, borderColor, textColor, fullWidth }) => {
   return (
     <button className={` flex justify-center items-center gap-2 px-7 py-4 font-montserrat text-lg leading-none border-2
     ${backgroundColor ? `${backgroundColor} ${borderColor} ${textColor}` :
-        'bg-coral-red text-white border-coral-red'} rounded-full`}>
+        'bg-coral-red text-white border-coral-red'} rounded-full ${fullWidth && 'w-full'} `}>
       {label}
 
       {iconUrl && <img src={iconUrl} alt="" className=' ml-2 rounded-full w-5 h-5' />}
